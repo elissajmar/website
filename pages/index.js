4c6628ba5c9c94b67698a2cf4752dfcd61b25f29
Elissa Martial <lmartial@hotmail.com>
2022-09-13 20:28:58 -0700

    new website update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className="">
-      <p>hello!</p>
+      <p>helllloooooooo!</p>
     </div>
   )
 }
